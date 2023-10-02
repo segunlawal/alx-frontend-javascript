58f1951eaebae2d3a9683f6c94afb089f184a92b
Segun <lawalsegun72@gmail.com>
2023-10-03 00:52:15 +0100

    last six tasks

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,6 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (let value of array) {
+    value = appendString + value;
+  }
+  return array;
+}
---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: function () {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
