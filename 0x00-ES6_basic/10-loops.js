8bcb06dc19bf37162b535cb6143be898e98617c6
Segun <lawalsegun72@gmail.com>
2023-10-03 00:58:36 +0100

    lint fix
    task 1 fix
    
    lint fix
    
    lint fix
    
    lint fix
    
    lint fix
    
    lint fix
    
    lint fix
    
    Update 12-createReportObject.js
    
    Update 12-createReportObject.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
+  const arr = [];
+  for (const value of array) {
+    arr.push(appendString + value);
   }
-  return array;
+  return arr;
 }
