07dd06d173a1ab077e7f6114dd5f674e6d6945b8
Segun <lawalsegun72@gmail.com>
2023-10-03 10:15:28 +0100

    add export statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((res) =>
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`),
