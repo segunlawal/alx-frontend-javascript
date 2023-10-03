0a8b850526741c710ca96537833d142f509a0d35
Segun <lawalsegun72@gmail.com>
2023-10-03 08:50:18 +0100

    add tasks

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI() {
   const b = 4;
   return new Promise((resolve, reject) => {
-    if (b == 4) {
+    if (b === 4) {
       resolve('Promise');
     } else {
       reject();
