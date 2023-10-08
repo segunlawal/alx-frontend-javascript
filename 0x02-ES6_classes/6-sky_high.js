e9c5186a113e678e62cb5c4130412b60e24f8eef
Segun <lawalsegun72@gmail.com>
2023-10-08 23:11:33 +0100

    task 7

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,9 +1,12 @@
 import Building from './5-building';
+
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
+
     this._floors = floors;
   }
+
   get floors() {
     return this._floors;
   }
