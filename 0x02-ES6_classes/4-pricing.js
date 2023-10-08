6eb05d9a1625b85905fcc6cd0c0c03be4e0e6213
Segun <lawalsegun72@gmail.com>
2023-10-08 22:46:00 +0100

    fix setters

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,28 +2,32 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of Currency');
-    }
     this._amount = amount;
     this._currency = currency;
   }
 
   get amount() {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
     return this._amount;
   }
 
   get currency() {
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of Currency');
-    }
     return this._currency;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      this._amount = value;
+    }
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('current must be a Currency object');
+    }
   }
 
   displayFullPrice() {
@@ -31,13 +35,6 @@ export default class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    if (typeof conversionRate !== 'number') {
-      throw new TypeError('Conversion Rate must be a number');
-    }
-
     return amount * conversionRate;
   }
 }
