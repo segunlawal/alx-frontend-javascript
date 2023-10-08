fda76cbda4e5a30e5ff2ee2e07d64ae7462ca3e0
Segun <lawalsegun72@gmail.com>
2023-10-08 22:10:10 +0100

    eslint fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -6,10 +6,7 @@ export default class HolbertonCourse {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    if (
-      !Array.isArray(students) ||
-      !students.every((student) => typeof student === 'string')
-    ) {
+    if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array of strings');
     }
     this._name = name;
