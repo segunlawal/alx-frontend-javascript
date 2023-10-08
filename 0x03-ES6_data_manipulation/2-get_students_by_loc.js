880cf57f898fe9f894b49c9b2fc80ef0a0bdee48
Segun <lawalsegun72@gmail.com>
2023-10-09 00:47:51 +0100

    filter arrray

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 import getListStudents from './0-get_list_students';
 
 export default function getStudentsByLocation(getListStudents, city) {
-  return getListStudents.filter(city);
+  return getListStudents.filter((student) => student.location === city);
 }
