3f128cc3df272246e742e1bac753b47eda750ea3
Segun <lawalsegun72@gmail.com>
2023-10-09 15:55:45 +0100

    task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  const filteredStudents = arr.filter((student) => student.city === city);
+  for (const student of filteredStudents) {
+    student.grade = 'N/A';
+  }
+  const updatedStudents = filteredStudents.map((student) => {
+    const gradeObject = newGrades.find(
+      (grade) => grade.studentId === student.studentId,
+    );
+    const grade = gradeObject ? gradeObject.grade : 'N/A';
+    return { ...student, grade };
+  });
+}
